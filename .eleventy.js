91460d6286841c3f50ab421ceb86d554d1045b32
Ashton Harris <ashtonharris.me@gmail.com>
2019-08-18 13:58:37 -0400

    Updating how files are copied

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,6 @@
 module.exports = function(eleventyConfig) {
-  eleventyConfig.addPassthroughCopy('src/img')
+  eleventyConfig.addPassthroughCopy('src/img');
+  eleventyConfig.addPassthroughCopy('src/fonts');
 
   return {
     dir: { input: 'src', output: 'dist' },
