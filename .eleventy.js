0793e31c7d493cf9ec97ef963da1c3c8520d3d64
Ashton Harris <ashtonharris.me@gmail.com>
2019-08-18 17:24:53 -0400

    Finalizing eleventy config

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy('src/img');
   eleventyConfig.addPassthroughCopy('src/fonts');
+  eleventyConfig.addPassthroughCopy('src/resources');
 
   return {
     dir: { input: 'src', output: 'dist' },
