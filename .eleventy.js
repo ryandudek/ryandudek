c863c96be7d26f92fd352b2e45aead9b21aea47a
Ryan Dudek <ryandudek@gmail.com>
2019-09-18 07:55:12 -0400

    Inline CSS

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,17 @@
+const CleanCSS = require("clean-css");
 module.exports = function(eleventyConfig) {
-  eleventyConfig.addPassthroughCopy('src/img');
-  eleventyConfig.addPassthroughCopy('src/fonts');
-  eleventyConfig.addPassthroughCopy('src/resources');
-  eleventyConfig.addPassthroughCopy('favicon.ico');
+    eleventyConfig.addPassthroughCopy('src/img');
+    eleventyConfig.addPassthroughCopy('src/fonts');
+    eleventyConfig.addPassthroughCopy('src/resources');
+    eleventyConfig.addPassthroughCopy('favicon.ico');
+    eleventyConfig.addFilter("cssmin", function(code) {
+        return new CleanCSS({}).minify(code).styles;
+    });
 
-  return {
-    dir: { input: 'src', output: 'dist' },
-    passthroughFileCopy: true,
-    templateFormats: ['njk', 'md', 'css', 'html', 'yml'],
-    htmlTemplateEngine: 'njk'
-  }
+    return {
+        dir: { input: 'src', output: 'dist' },
+        passthroughFileCopy: true,
+        templateFormats: ['njk', 'md', 'css', 'html', 'yml'],
+        htmlTemplateEngine: 'njk'
+    }
 }
